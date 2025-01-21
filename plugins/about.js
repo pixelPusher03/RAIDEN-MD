ade56a3f8be754558d7e4c7f4c8b05460eeac2cd
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2025-01-21 21:11:37 +0300

    Add files via upload

--- plugins/about.js
+++ plugins/about.js
@@ -1,1 +1,44 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    react: "👑",
+    desc: "get owner dec",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `*╭┈───────────────•*
 
+*👋 HELLO ${pushname}*
+
+*╰┈───────────────•*
+*╭┈───────────────•*
+*│  ◦* *ᴡᴇʟᴄᴏᴍᴇ ɪᴛs raiden-ʙᴏᴛ*
+*│  ◦* *ʀᴇᴀʟ ɴᴀᴍᴇ➠ The Developer03*
+*│  ◦* *ᴘᴜʙʟɪᴄ ɴᴀᴍᴇ➠ Dev Codez*
+*│  ◦* *ᴀɢᴇ➠ 300000 years*
+*│  ◦* *ᴄɪᴛʏ➠ Earth 350*
+*│  ◦* *ᴀ sɪᴍᴘʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ᴅᴇᴠᴇʟᴘᴏʀ*
+*╰┈───────────────•*
+> *◆◆◆◆◆◆◆◆◆◆◆◆*
+
+*[ • RAIDEN - TEAM • ]*
+*╭┈───────────────•*
+*│  ◦* *▢➠The Developer03*
+*│  ◦* *▢➠Pixel Pusher03*
+*│  ◦* *▢➠Byte Smith03*
+*╰┈───────────────•*
+*•────────────•⟢*
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛᴀʟᴋᴅʀᴏᴠᴇ
+*•────────────•⟢*
+`
+
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:about},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
